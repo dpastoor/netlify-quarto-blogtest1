d570fe0ad737a1e497e5b8392d5791f2f64f619f
Devin Pastoor <devin.pastoor@gmail.com>
2022-02-23 10:21:59 -0500

    ci: add redirect so script doesn't download html page

--- _build.sh
+++ _build.sh
@@ -4,13 +4,13 @@ set -ex
 
 QUARTO_VERSION=0.9.30
 
-curl "https://github.com/quarto-dev/quarto-cli/releases/download/v$QUARTO_VERSION/quarto-$QUARTO_VERSION-linux-amd64.deb" -o /tmp/quarto.deb
+curl -L "https://github.com/quarto-dev/quarto-cli/releases/download/v$QUARTO_VERSION/quarto-$QUARTO_VERSION-linux-amd64.deb" -o /tmp/quarto.deb
 
 dpkg -x /tmp/quarto.deb /tmp
 
-# quarto unpacks to /opt/quart/bin 
+# quarto unpacks to /opt/quart/bin
 
-# default netlify repo dir
+#default netlify repo dir
 cd /opt/build/repo
 
 /tmp/opt/quarto/bin/quarto render
