7a0b2e5914f7acc9746cd5685cb403814007fde3
Devin Pastoor <devin.pastoor@gmail.com>
2022-02-23 10:24:16 -0500

    ci: attempt to work around permissions issue of just unpacking to /tmp
    failure was:
    
    10:22:16 AM: + dpkg -x /tmp/quarto.deb /tmp
    10:22:18 AM: tar: .: Cannot utime: Operation not permitted
    10:22:18 AM: tar: .: Cannot change mode to rwxr-xr-x: Operation not permitted

--- _build.sh
+++ _build.sh
@@ -6,12 +6,14 @@ QUARTO_VERSION=0.9.30
 
 curl -L "https://github.com/quarto-dev/quarto-cli/releases/download/v$QUARTO_VERSION/quarto-$QUARTO_VERSION-linux-amd64.deb" -o /tmp/quarto.deb
 
-dpkg -x /tmp/quarto.deb /tmp
+mkdir /tmp/quarto
+
+dpkg -x /tmp/quarto.deb /tmp/quarto
 
 # quarto unpacks to /opt/quart/bin
 
 #default netlify repo dir
 cd /opt/build/repo
 
-/tmp/opt/quarto/bin/quarto render
+/tmp/quarto/opt/quarto/bin/quarto render
 
